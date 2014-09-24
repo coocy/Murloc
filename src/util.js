f06b1d010e038bd25f9f0586d67b339f8f3646e9
binnng <binnng@163.com>
2014-09-24 20:23:16 +0800

    Update util.js
    完善下注释。：）

--- src/util.js
+++ src/util.js
@@ -1,6 +1,8 @@
 /**
- * 工具类
- * author binnng
+ * 加载js
+ * @param {String} url js文件路径
+ * @param {Function} callback 加载完成后的回调
+ * @param {Array} opts 传给回调的参数
  */
 
 $.getScript = function(url, callback, opts) {
@@ -27,6 +29,10 @@ $.getScript = function(url, callback, opts) {
 	elHead.appendChild(elScript);
 };
 
+/**
+ * 加载css
+ * @param {String} url css文件路径
+ */
 $.getCSS = function(url) {
 	var elHead = DOC.getElementsByTagName('head')[0] || DOC.body,
 		elLink = DOC.createElement('link');
